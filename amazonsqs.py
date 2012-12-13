d09523ca869eb3eb8f18138b9192f1d9a9281b03
Mario J. Inga Cahuana <mario21ic@gmail.com>
2012-12-13 15:54:36 -0500

    Lectura, escritura y borrado

---
+++ amazonsqs.py
@@ -0,0 +1,53 @@
+from boto.sqs.connection import SQSConnection
+from boto.sqs.message import Message
+import os, ConfigParser
+import simplejson as json
+
+class SQS:
+    def __init__(self):
+        ruta = os.path.dirname(os.path.realpath(__file__))
+        config = ConfigParser.ConfigParser()
+        config.read(str(ruta) + "/config.ini")
+
+        aws_access_key_id = config.get("amazonsqs", "access_key_id")
+        aws_secret_access_key = config.get("amazonsqs", "secret_access_key")
+        aws_queue = config.get("amazonsqs", "queue")
+
+        try:
+            self.conn = SQSConnection(aws_access_key_id, aws_secret_access_key)
+            self.set_queue(aws_queue)
+        except:
+            print 'Error connection'
+
+    def get_all_queues(self):
+        return self.conn.get_all_queues()
+
+    def create_queue(self, queue, timeout):
+        return self.conn.create_queue(queue, timeout)
+
+    def set_queue(self, queue):
+        self.queue = self.conn.get_queue(queue)
+        return True
+
+    def get_messages(self, limit=10):
+        return self.queue.get_messages(limit)
+
+    def count(self):
+        #print "Count: %s" % self.queue.count()
+        return self.queue.count()
+
+    def write(self, data):
+        m = Message()
+        m.set_body(json.dumps(data))
+        return self.queue.write(m)
+
+    def delete(self, id):
+        #print "Eliminando %s" % id
+        self.cola.delete_message(id)
+
+    def clear(self):
+        return self.queue.clear()
+
+    def delete_queue(self):
+        return self.conn.delete_queue(self.queue)
+
---
+++ main.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+
+from amazonsqs import SQS
+amazon_sqs = SQS()
+
+while 1:
+    print "---------------------------"
+    print "-     AmazonSQS           -"
+    print "---------------------------"
+    print "- [N] Nuevo               -"
+
+    colas = amazon_sqs.get_all_queues()
+    for index in range(len(colas)):
+        print "- [%s] %s" % (index, colas[index].name)
+
+    print "---------------------------"
+    print "- [Q] Salir               -"
+    print "---------------------------"
+
+    action = raw_input("Seleccione una accion o cola: ")
+
+    if action == "Q" or action == "q":
+        print "Bytez"
+        exit()
+
+    elif action == "N" or action == "n":
+        queue_name = raw_input("Nombre del nuevo queue: ")
+        queue_timeout = raw_input("Timeout del nuevo queue: ")
+        try:
+            amazon_sqs.create_queue(queue_name, queue_timeout)
+        except:
+            print "Error creando queue"
+
+    else:
+        try:
+            cola = colas[int(action)].name
+            amazon_sqs.set_queue(cola)
+        except:
+            print "Error al seleccionar la cola"
+            exit()
+
+        print "---------------------------"
+        print "-        %s" % cola
+        print "---------------------------"
+        print "- [N] Nuevo mensaje       -"
+        print "- [L] Leer mensajes       -"
+        print "- [B] Borrar mensaje      -"
+        print "---------------------------"
+        print "- [I] Detalles de queue   -"
+        print "- [V] Vaciar queue        -"
+        print "- [D] Eliminar queue      -"
+        print "---------------------------"
+        print "- [Q] Salir               -"
+        print "---------------------------"
+
+        action = raw_input("Seleccione una acción: ")
+
+        # Nuevo mensaje
+        if action == "N" or action == "n":
+            data = raw_input("Escriba la data a enviar: ")
+            try:
+                amazon_sqs.write(data)
+                print "Datos escritos: %s" % data
+            except:
+                print "Error escribiendo: %s" % data
+
+        # Leer mensajes
+        elif action == "L" or action == "l":
+            mensajes = amazon_sqs.get_messages()
+            print "%s mensajes en %s" % (len(mensajes), cola)
+            for mensaje in mensajes:
+                print "id: %s - mensaje: %s" % (mensaje.id, mensaje.get_body())
+
+        # Vaciar queue
+        elif action == "V" or action == "v":
+            rondas = raw_input("Escriba el número de intentos (10 mensajes x intento): ");
+
+            try:
+                mensajes = amazon_sqs.count()
+                print "%s mensajes a eliminar" % mensajes
+
+                if mensajes>0:
+                    amazon_sqs.clear()
+                    print "Queue limpio"
+
+            except:
+                print "Error limpiando"
+
+        # Eliminar queue
+        elif action == "D" or action == "d":
+            confirmar = raw_input("¿Está seguro que desea eliminar la cola? [S/N]: ")
+            if confirmar == "S" or confirmar == "s":
+                try:
+                    amazon_sqs.delete_queue()
+                    print "Queue %s eliminado" % cola
+                except:
+                    print "Error eliminando %s" % cola
+
+        else:
+            print "Operación inválida"
